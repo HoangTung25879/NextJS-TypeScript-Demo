c9564369065a86bc4a3377725048c47afde08617
HOANG TUNG <tungphamkh@itrvn.com>
2022-08-15 21:50:48 +0700

    no message

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,11 +3,8 @@ import React from 'react';
 const Login = () => {
   return (
     <div>
-<<<<<<< HEAD
       zzxxz
-=======
       ss
->>>>>>> bc257e447dc926cb179559e6669316451e1d8789
     </div>
   );
 };
